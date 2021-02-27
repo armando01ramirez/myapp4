d33f5a9c6801ccc89cceb6977692076aa36d1047
armando01ramirez <azb619@hotmail.com>
2021-02-27 16:44:28 -0600

    trabajo my app4

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MiFormulario from './componente/formulario';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Clase Semana 3 Trabajando con Formularios Hook</h1>
+      <MiFormulario />
+    </div>
+  );
+}
+
+export default App;
